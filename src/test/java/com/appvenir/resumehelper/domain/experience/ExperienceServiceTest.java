3f194c70f2f0c45e95168ca156ace4a6a6ef8621
Frederic <olivierjp@protonmail.com>
2024-08-03 22:54:37 -0400

    Creating unite test for experienceService

---
+++ src/test/java/com/appvenir/resumehelper/domain/experience/ExperienceServiceTest.java
@@ -0,0 +1,10 @@
+package com.appvenir.resumehelper.domain.experience;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ExperienceServiceTest {
+
+    
+    
+}
