99649f1bfb24bd6391dd1dfaa900128431aa93e4
Frederic <olivierjp@protonmail.com>
2024-09-07 16:17:10 -0400

    Rename app to rezulab

--- src/test/java/com/appvenir/resumehelper/App.java
+++ src/test/java/com/appvenir/resumehelper/App.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ResumehelperApplicationTests {
+class App {
 
 	@Test
 	void contextLoads() {
