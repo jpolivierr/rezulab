94d321e1108430e26248440c080ca62e6455cddb
Frederic <olivierjp@protonmail.com>
2024-09-23 09:18:10 -0400

    Add null check in updatejobListing method

--- src/main/java/com/appvenir/resumehelper/domain/jobListings/controller/JobListingController.java
+++ src/main/java/com/appvenir/resumehelper/domain/jobListings/controller/JobListingController.java
@@ -2,11 +2,16 @@ package com.appvenir.resumehelper.domain.jobListings.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.appvenir.resumehelper.domain.jobListings.dto.JobListingDto;
@@ -24,11 +29,31 @@ public class JobListingController {
 
     @PostMapping
     public ResponseData<JobListingDto> createJobListing(
-        @RequestBody JobListingDto jobListingObject,
-        @RequestParam("email") String email)
+    @RequestBody JobListingDto jobListingObject,
+    @RequestParam("email") String email)
+    {
+        var jobListing = jobListingService.createJobListing(email, jobListingObject);
+        return ResponseData.set(jobListing);
+    }
+
+    @PutMapping("/{jobListingId}")
+    public ResponseData<JobListingDto> updateJobListing(
+        @RequestBody JobListingDto jobListingDto,
+        @PathVariable("jobListingId") Long jobListingId,
+        @RequestParam("email") String email
+    )
+    {
+        var jobListing = jobListingService.updateJobListing(email, jobListingId, jobListingDto);
+        return ResponseData.set(jobListing);
+    }
+    
+    @GetMapping("/{jobListingId}")
+    public ResponseData<JobListingDto> getJobListing(
+        @RequestParam("email") String email,
+        @PathVariable("jobListingId") Long jobListingId
+        )
         {
-            var jobListing = jobListingService.createJobListing(email, jobListingObject);
-            return ResponseData.set(jobListing);
+            return ResponseData.set(jobListingService.getJobListing(email, jobListingId));
         }
     
     @GetMapping
@@ -38,4 +63,14 @@ public class JobListingController {
             var jobListings = jobListingService.getAllJobListing(email);
             return ResponseData.set(jobListings);
         }
+
+    @DeleteMapping("/{jobListingId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteJobListing(
+        @RequestParam("email") String email,
+        @PathVariable("jobListingId") Long jobListingId
+        )
+        {
+            jobListingService.deleteJobListing(email, jobListingId);
+        }    
 }
--- src/main/java/com/appvenir/resumehelper/domain/jobListings/repository/JobListingRepository.java
+++ src/main/java/com/appvenir/resumehelper/domain/jobListings/repository/JobListingRepository.java
@@ -1,11 +1,15 @@
 package com.appvenir.resumehelper.domain.jobListings.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.appvenir.resumehelper.domain.jobListings.model.JobListing;
+import com.appvenir.resumehelper.domain.user.model.User;
 
 public interface JobListingRepository extends JpaRepository<JobListing,Long> {
     List<JobListing> findAllByUserEmail(String email);
+    Optional<JobListing> findByIdAndUser(Long id, User user);
+    void deleteByIdAndUser(Long id, User user);
 }
